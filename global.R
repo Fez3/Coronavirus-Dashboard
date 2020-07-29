6bb58a7d87008b7dccba0f6dd36e86af53a571f7
Fez3 <35278600+Fez3@users.noreply.github.com>
2020-07-29 19:16:56 -0400

    First Working Version

--- global.R
+++ global.R
@@ -1,15 +1,63 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
-
+library(leaflet)
+library(shinydashboard)
 library(readr)
 library(knitr)
 
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(data.table)
+library(tidyr)
+library(gganimate)
+library(leaflet)
+library(geojsonio)
+library(maps)
+library(htmlwidgets)
+library(htmltools)
+library(rgdal)
+library(zoo)
+library(latticeExtra)
+library(shinydashboard)
+library(googleVis)
+library(RColorBrewer)
+library(widgetframe)
+library(scales)
+installations <- data.frame("Name" = c("Austin Installation", "Aberdeen Installation", "Leavenworth Installation"),
+                            "lat" = c(30.26897, 39.42535, 39.34798), "lng" = c(-97.74081,-76.21358,-94.93554 ))
 myfile <- "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 confirmed <- read_csv(myfile)
 inst1CC <- confirmed[confirmed$Combined_Key == "Travis, Texas, US" ,11:ncol(confirmed)]
 inst2CC <- confirmed[confirmed$Combined_Key == "Harford, Maryland, US" ,11:ncol(confirmed)]
 inst3CC <- confirmed[confirmed$Combined_Key == "Leavenworth, Kansas, US" ,11:ncol(confirmed)]
+myfile <- paste("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_daily_reports/", format(Sys.Date()-1, "%m-%d-%Y"),".csv",sep="")
+incidence <- read_csv(myfile)
+installations = installations %>% mutate(Incidence_Rate = c(
+incidence[incidence$Combined_Key == "Travis, Texas, US",]$Incidence_Rate,
+incidence[incidence$Combined_Key == "Harford, Maryland, US",]$Incidence_Rate,
+incidence[incidence$Combined_Key == "Leavenworth, Kansas, US",]$Incidence_Rate))
 myfile <- "https://opendata.arcgis.com/datasets/6ac5e325468c4cb9b905f1728d6fbf0f_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D"
 capacity <- read_csv(myfile)
-inst1Beds = aggregate(capacity[capacity$STATE=="TX" & capacity$COUNTY=="TRAVIS"]$BEDS)
+installations = installations %>% mutate(Beds = c(
+sum(capacity[capacity$STATE=="TX" & capacity$COUNTY=="TRAVIS",]$BEDS),
+sum(capacity[capacity$STATE=="MD" & capacity$COUNTY=="HARFORD",]$BEDS),
+sum(capacity[capacity$STATE=="KS" & capacity$COUNTY=="LEAVENWORTH",]$BEDS)))
+myfile<- "https://covidtracking.com/api/v1/states/tx/daily.csv"
+testing <- read_csv(myfile)
+testing = filter(testing, testing$totalTestResultsIncrease >0)
+testCap1 = mean(Map("/",testing[1:7, "positiveIncrease"], testing[1:7, "totalTestResultsIncrease"])[[1]])
+tests1 = mean(testing[1:7, "positiveIncrease"][[1]])
+myfile<- "https://covidtracking.com/api/v1/states/md/daily.csv"
+testing <- read_csv(myfile)
+testing = filter(testing, testing$totalTestResultsIncrease >0)
+testCap2 = mean(Map("/",testing[1:7, "positiveIncrease"], testing[1:7, "totalTestResultsIncrease"])[[1]])
+tests2 = mean(testing[1:7, "positiveIncrease"][[1]])
+myfile<- "https://covidtracking.com/api/v1/states/ks/daily.csv"
+testing <- read_csv(myfile)
+testing = filter(testing, testing$totalTestResultsIncrease >0)
+testCap3 = mean(Map("/",testing[1:7, "positiveIncrease"], testing[1:7, "totalTestResultsIncrease"])[[1]])
+tests3 = mean(testing[1:7, "positiveIncrease"][[1]])
+installations = installations %>% mutate(Testing_Ratio = c(testCap1, testCap2, testCap3))
+installations = installations %>% mutate(Testing_Total = c(tests1, tests2, tests3))
--- ui.R
+++ ui.R
@@ -1,7 +1,7 @@
 
  
 shinyUI(dashboardPage(skin = "green",
-  dashboardHeader(title = "Contagion Spread"),
+  dashboardHeader(title = "Installations"),
   
   dashboardSidebar(Title="Aaron Festinger", br(), 
                    img(src="A.jpg", width=100,height = 100, align="center"),
@@ -9,8 +9,8 @@ shinyUI(dashboardPage(skin = "green",
         sidebarMenu(
               
           menuItem("Map", tabName = "map", icon = icon("map")),
-          menuItem("Data", tabName = "data", icon = icon("database")),
-          menuItem("State by State", tabName = "statedata", icon = icon("database")),
+          #menuItem("Data", tabName = "data", icon = icon("database")),
+          #menuItem("State by State", tabName = "statedata", icon = icon("database")),
           menuItem("Geographic Spread", tabName = "hmap", icon = icon("database"))                          
                                     )
                                     ),
@@ -20,16 +20,10 @@ shinyUI(dashboardPage(skin = "green",
     tabItems(
       tabItem(tabName = "map",
               leafletOutput("map"),box(
-              selectizeInput(inputId = "disease",label = "Contagion",choices = Diseases), 
-              sliderInput(inputId="Date", label="Date", as.Date(min(data$TIME)), as.Date(max(data$TIME)),value=as.Date(min(data$TIME)),
-                                                  timeFormat="%Y-%m-%d" ,animate = animationOptions(interval = 100   ))
-            )  ),
-      tabItem(tabName = "data",
-              htmlOutput("peak"),selectizeInput(inputId = "disease",label = "Contagion",choices = Diseases),plotOutput ("heat")
-              ),
-      
-      tabItem(tabName = "statedata", box(selectizeInput(inputId = "disease",label = "Contagion",choices = Diseases),selectizeInput(inputId = "stin",label = "State",choices = stt)),column(width = 4, htmlOutput("byState")
-     ) ),
+                selectizeInput(inputId = "inst",label = "Installation",choices = installations$Name)),
+  
+      box(
+        textOutput("tstCR"))),
       
       
       tabItem(tabName = "hmap", box(tags$iframe(src="https://www.youtube.com/embed/dQw4w9WgXcQ",width="600",height="400") )
